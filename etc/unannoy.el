9f2383567face47488c4c6293c15600851b34eda
Lorenzo David <lorenzodavid91@gmail.com>
2015-10-10 16:24:23 -0700

    Add unannoy features

---
+++ etc/unannoy.el
@@ -0,0 +1,46 @@
+;;; unannoy.el --- disable Emacs' annoying bits
+
+;;; Code:
+
+;; GUIs are for newbs
+(menu-bar-mode -1)
+(if (fboundp 'tool-bar-mode) (tool-bar-mode -1))
+(if (fboundp 'scroll-bar-mode) (scroll-bar-mode -1))
+
+;; Too distracting
+(blink-cursor-mode -1)
+
+;; Don't jerk me around
+(electric-indent-mode -1)
+
+;; I never want to use this
+(when (fboundp 'set-horizontal-scroll-bar-mode)
+  (set-horizontal-scroll-bar-mode nil))
+
+;; I hate typing
+(defalias 'yes-or-no-p 'y-or-n-p)
+
+;; Always use the one true encoding
+(prefer-coding-system 'utf-8-unix)
+
+;; Magit is the only front-end I care about
+(setf vc-handled-backends nil
+      vc-follow-symlinks t)
+
+;; Stop scrolling by huge leaps
+(setq mouse-wheel-scroll-amount '(1 ((shift) . 1))
+      scroll-step 1
+      scroll-preserve-screen-position t)
+(global-set-key (kbd "M-n") (kbd "C-u 1 C-v"))
+(global-set-key (kbd "M-p") (kbd "C-u 1 M-v"))
+
+;; Full screen
+(toggle-frame-fullscreen)
+
+;; window moving
+(windmove-default-keybindings)
+;; (setq org-replace-disputed-keys t) ;; org-mode fix
+
+(provide 'unannoy)
+
+;;; unannoy.el ends here
--- init.el
+++ init.el
@@ -1,1 +1,24 @@
 ;;; init.el
+
+;;; Code:
+
+(add-to-list 'load-path "~/.emacs.d/etc")
+
+;; Load local "packages"
+(require 'unannoy)
+
+;; elpa
+(require 'package)
+(package-initialize) ;; add all installed packages to the load path
+(add-to-list 'package-archives
+             '("melpa-stable" .
+	       "http://stable.melpa.org/packages/") t)
+
+;; Magit
+(global-set-key (kbd "C-x g") 'magit-status)
+
+;; Compile configuration
+(byte-recompile-directory "~/.emacs.d/etc/" 0)
+(byte-recompile-file "~/.emacs.d/init.el" nil 0)
+
+(provide 'init) ; make (require 'init) happy
